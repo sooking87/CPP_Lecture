f3337c0a6bba631b1fbc0331692a9f1ffcde2c24
sooking87 <sksohn01@naver.com>
2022-12-13 06:10:10 +0900

    2022-12-13-laptop

--- Test/temp.cpp
+++ Test/temp.cpp
@@ -5,179 +5,150 @@
 
 using namespace std;
 
-class Util
+class OutOfRange
 {
-public:
-    static bool isDiv(int num1, int num2);
-    static bool isPrime(int num);
 };
-class P
+class Path
 {
-protected:
-    int n;
+private:
+    int x1, y1, x2, y2;
 
 public:
-    P(int n);
-    virtual int solution() = 0;
+    Path(int xx1, int yy1, int xx2, int yy2);
+    friend bool operator==(Path &p1, Path &p2);
 };
-class P8 : public P
+class PathQue
 {
-public:
-    P8(int n);
-    virtual int solution();
-};
-class P91 : public P
-{
-protected:
-    int m;
+private:
+    vector<Path> route;
+    bool isIn(Path &p);
 
 public:
-    P91(int n);
-    virtual int solution();
+    void push(Path &p);
+    int count();
 };
-class P92 : public P
+class MyGame
 {
-protected:
-    int m;
+private:
+    int x, y;
+    void move(char c);
+    PathQue myque;
 
 public:
-    P92(int n);
-    virtual int solution();
+    MyGame();
+    void move(string &commands);
+    friend ostream &operator<<(ostream &os, MyGame &game);
 };
-class P10 : public P
+/* Path 정의 */
+Path::Path(int xx1, int yy1, int xx2, int yy2)
 {
-public:
-    P10(int n);
-    virtual int solution();
-};
-/* P 정의 */
-P::P(int n)
-{
-    this->n = n;
+    x1 = xx1;
+    y1 = yy1;
+    x2 = xx2;
+    y2 = yy2;
 }
-/* P8 정의 */
-P8::P8(int n) : P(n)
+bool operator==(Path &p1, Path &p2)
 {
-    ;
-}
-int P8::solution()
-{
-    int returnAns = 0;
-    for (int i = 2; i <= n; i++)
-    {
-        if (Util::isPrime(i))
-        {
-            returnAns++;
-        }
-    }
-    return returnAns;
-}
-/* P9 정의 */
-P91::P91(int n, int m) : P(n)
-{
-    this->m = m;
-}
-P92::P92(int n, int m) : P(n)
-{
-    this->m = m;
-}
-int P91::solution()
-{
-    // 최대 공약수 구하기
-    int gcd = 0;
-    int min = n > m ? m : n;
-    int max = n > m ? n : m;
-
-    for (int i = min; i > 0; i--)
-    {
-        if (Util::isDiv(min, i) && Util::isDiv(max, i))
-        {
-            gcd = i;
-            break;
-        }
-    }
-    return gcd;
-}
-/* P10 정의 */
-P10::P10(int n) : P(n)
-{
-    ;
-}
-int P10::solution()
-{
-    int returnNum = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            returnNum += i;
-            while (n % i != 0)
-            {
-                n = n / i;
-            }
-        }
-    }
-    return returnNum;
-}
-/* Util 정의 */
-bool Util::isDiv(int num1, int num2)
-{
-    if (num1 % num2 == 0)
-    {
+    if ((p1.x1 == p2.x1) && (p1.y1 == p2.y1) && (p1.x2 == p2.y2) && (p1.y2 == p2.y2))
         return true;
-    }
-    else
+    if ((p1.x1 == p2.x2) && (p1.y1 == p2.y2) && (p1.x2 == p2.y1) && (p1.y2 == p2.y1))
+        return true;
+
+    return false;
+}
+/* PathQue 정의 */
+void PathQue::push(Path &p)
+{
+
+    if (!isIn(p))
     {
-        return false;
+        route.push_back(p);
     }
 }
-bool Util::isPrime(int num)
+bool PathQue::isIn(Path &p)
 {
-    int i;
-    for (i = 2; i < num; i++)
+    for (int i = 0; i < route.size(); i++)
     {
-        if (num % i == 0)
+        if (route[i] == p)
+            return true;
+    }
+    return false;
+}
+int PathQue::count()
+{
+    return route.size();
+}
+/* MyGame 정의 */
+MyGame::MyGame()
+{
+    this->x = 0;
+    this->y = 0;
+}
+void MyGame::move(char c)
+{
+    switch (c)
+    {
+    case 'U':
+        if (y == 5)
+            throw OutOfRange();
+        y++;
+        break;
+    case 'D':
+        if (y == -5)
+            throw OutOfRange();
+        y--;
+        break;
+    case 'R':
+        if (x == 5)
+            throw OutOfRange();
+        x++;
+        break;
+    case 'L':
+        if (x == -5)
+            throw OutOfRange();
+        x--;
+        break;
+    default:
+        throw OutOfRange();
+    }
+}
+void MyGame::move(string &commands)
+{
+    for (int i = 0; i < commands.size(); i++)
+    {
+        try
         {
-            break;
+            int prevX = x;
+            int prevY = y;
+            move(commands[i]);
+            Path p(prevX, prevY, x, y);
+            myque.push(p);
+        }
+        catch (const OutOfRange &e)
+        {
         }
     }
-    if (i == num)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+}
+ostream &operator<<(ostream &os, MyGame &game)
+{
+    os << "(" << game.x << ", " << game.y << ") -> " << game.myque.count() << endl;
+
+    return os;
 }
 int main()
 {
-    P *first, *second;
-    first = new P8(12);
-    second = new P8(5);
-    cout << "P8, 12: " << first->solution() << endl;
-    cout << "P8, 5: " << second->solution() << endl;
-    delete first;
-    delete second;
+    MyGame game;
+    string commands = "ULURRDLLU"; //"LULLLLLLU" "ULURRDLLU"
+    game.move(commands);
+    cout << game;
 
-    first = new P10(12);
-    second = new P10(5);
-    cout << "p10, 12: " << first->solution() << endl;
-    cout << "p10, 5: " << second->solution() << endl;
-    delete first;
-    delete second;
-
-    first = new P9(12, 8);
-    second = new P9(12, 6);
-    cout << "p9, 12, 8: " << first->solution() << endl;
-    cout << "p9, 12, 6: " << second->solution() << endl;
-    delete first;
-    delete second;
-
-    first = new P92(12, 8);
-    second = new P92(12, 6);
-    cout << "p92, 12, 8: " << first->solution() << endl;
-    cout << "p92, 12, 6: " << second->solution() << endl;
-    delete first;
-    delete second;
+    Path path1(1, 2, 3, 4);
+    Path path2(3, 4, 1, 2);
+    Path path3(1, 2, 5, 6);
+    // cout << (path1 == path2) << endl; // operator overloading 함수 정의 확인
+    PathQue myque;
+    myque.push(path1);
+    myque.push(path2);
+    myque.push(path3);
+    cout << myque.count() << endl;
 }
